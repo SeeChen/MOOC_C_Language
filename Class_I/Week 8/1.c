823683e2c0fc6a092244f3a21185db9ce7108caa
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-09-09 23:07:53 +0800

    None

--- Class_I/Week 8/1.c
+++ Class_I/Week 8/1.c
@@ -18,29 +18,23 @@ C++[回车]
 C[回车]
 BASIC[回车]
 */
-#include<stdio.h>
-#include<string.h>
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+int main(){
 	char a[6][20];
-	for(int i=0;i<5;i++)
-	{
+	for(int i=0;i<5;i++){
 		scanf("%s",a[i]);
 	}
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4-i;j++)
-		{
-			if(strcmp(a[j],a[j+1])<0)
-			{
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4-i;j++){
+			if(strcmp(a[j],a[j+1])<0){
 				strcpy(a[5],a[j]);
 				strcpy(a[j],a[j+1]);
 				strcpy(a[j+1],a[5]);
 			}
 		}
 	}
-	for(int i=0;i<5;i++)
-	{
+	for(int i=0;i<5;i++){
 		printf("%s\n",a[i]); 
 	}
 	return 0;
